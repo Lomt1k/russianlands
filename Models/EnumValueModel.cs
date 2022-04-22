dc07fbf34a850533567f8693c4f16cfb4ad93118
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-04-22 07:14:58 +0300

    Edit Item Window (in progress)

---
+++ Models/EnumValueModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace TextGameRPG.Models
+{
+    internal class EnumValueModel<T> where T : Enum
+    {
+        public string name;
+        public T value;
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public static ObservableCollection<EnumValueModel<T>> CreateCollection(IEnumerable<T> excludeValues)
+        {
+            var names = Enum.GetNames(typeof(T));
+            var values = Enum.GetValues(typeof(T));
+
+            var list = new List<EnumValueModel<T>>(names.Length);
+            for (int i = 0; i < names.Length; i++)
+            {
+                var item = new EnumValueModel<T>
+                {
+                    name = names[i],
+                    value = (T)values.GetValue(i)
+                };
+                list.Add(item);
+            }
+
+            foreach (var excludeValue in excludeValues)
+            {
+                var elementToRemove = list.Where(x => x.value.Equals(excludeValue)).First();
+                list.Remove(elementToRemove);
+            }
+
+            return new ObservableCollection<EnumValueModel<T>>(list);
+        }
+
+        public static ObservableCollection<EnumValueModel<T>> CreateCollection(T excludeValue)
+        {
+            return CreateCollection(new T[] { excludeValue });
+        }
+
+        public static ObservableCollection<EnumValueModel<T>> CreateCollection()
+        {
+            return CreateCollection(new T[] { });
+        }
+
+        public static EnumValueModel<T> GetModel(ObservableCollection<EnumValueModel<T>> collection, T enumValue)
+        {
+            foreach (var element in collection)
+            {
+                if (element.value.Equals(enumValue))
+                {
+                    return element;
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
--- ViewModels/Editor/ItemsEditor/EditItemWindowViewModel.cs
+++ ViewModels/Editor/ItemsEditor/EditItemWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Reactive;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
 using ReactiveUI;
+using TextGameRPG.Models;
+using TextGameRPG.Scripts.GameCore.Items;
 using TextGameRPG.Scripts.GameCore.Items.ItemGenerators;
 using TextGameRPG.Views.Editor.ItemsEditor;
 
@@ -7,8 +10,34 @@ namespace TextGameRPG.ViewModels.Editor.ItemsEditor
 {
     internal class EditItemWindowViewModel : ViewModelBase
     {
-        public ItemGeneratorBase editableItem { get; }
+        private EnumValueModel<ItemType> _selectedItemType;
+        private EnumValueModel<ItemRarity> _selectedItemRarity;
 
+        public ItemGeneratorBase editableItem { get; }
+        public ObservableCollection<EnumValueModel<ItemType>> itemTypeList { get; }
+        public ObservableCollection<EnumValueModel<ItemRarity>> itemRarityList { get; }
+
+        public EnumValueModel<ItemType> selectedItemType
+        {
+            get => _selectedItemType;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _selectedItemType, value);
+                editableItem.itemType = value.value;
+            }
+        }
+
+        public EnumValueModel<ItemRarity> selectedItemRarity
+        {
+            get => _selectedItemRarity;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _selectedItemRarity, value);
+                editableItem.itemRarity = value.value;
+            }
+        }
+
+        private System.Action closeWindow { get; }
         public ReactiveCommand<Unit, Unit> addPropertyCommand { get; }
         public ReactiveCommand<Unit, Unit> removePropertyCommand { get; }
         public ReactiveCommand<Unit, Unit> saveCommand { get; }
@@ -16,12 +45,21 @@ namespace TextGameRPG.ViewModels.Editor.ItemsEditor
 
         public EditItemWindowViewModel(EditItemWindow window, ItemGeneratorBase item)
         {
-            editableItem = item.Clone();
+            window.Title = $"[ID {item.id}] {item.debugName}";
 
+            editableItem = item.Clone();
+            var excludeTypes = new ItemType[] { ItemType.Any };
+            itemTypeList = EnumValueModel<ItemType>.CreateCollection(excludeTypes);
+            itemRarityList = EnumValueModel<ItemRarity>.CreateCollection();
+
+            _selectedItemType = EnumValueModel<ItemType>.GetModel(itemTypeList, editableItem.itemType);
+            _selectedItemRarity = EnumValueModel<ItemRarity>.GetModel(itemRarityList, editableItem.itemRarity);
+
+            closeWindow = () => window.Close();
             addPropertyCommand = ReactiveCommand.Create(AddNewProperty);
             removePropertyCommand = ReactiveCommand.Create(RemoveSelectedProperty);
-            saveCommand = ReactiveCommand.Create(SaveItemChanges);
-            cancelCommand = ReactiveCommand.Create(() => window.Close());
+            saveCommand = ReactiveCommand.Create(SaveItemChanges + closeWindow);
+            cancelCommand = ReactiveCommand.Create(closeWindow);
         }
 
         private void AddNewProperty()
@@ -36,7 +74,8 @@ namespace TextGameRPG.ViewModels.Editor.ItemsEditor
 
         private void SaveItemChanges()
         {
-            //TODO
+            var dataBase = Scripts.GameCore.GameDataBase.GameDataBase.instance.itemsGenerators;
+            dataBase.ChangeData(editableItem.id, editableItem);
         }
 
     }
