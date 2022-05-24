8c6fb4be27ddde68bbb83abf4e563a81057df58c
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-05-24 08:24:25 +0300

    small localization update

--- Scripts/TelegramBot/Dialogs/Town/Character/InventoryDialog.cs
+++ Scripts/TelegramBot/Dialogs/Town/Character/InventoryDialog.cs
@@ -7,27 +7,27 @@ namespace TextGameRPG.Scripts.TelegramBot.Dialogs.Town.Character
     {
         public InventoryDialog(GameSession _session) : base(_session)
         {
-            RegisterButton($"{Emojis.inventory[Inventory.Sword]} " + Localization.Get(session, "inventory_category_swords"),
+            RegisterButton($"{Emojis.inventory[Inventory.Sword]} " + Localization.Get(session, "menu_item_swords"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Bow]} " + Localization.Get(session, "inventory_category_bows"),
+            RegisterButton($"{Emojis.inventory[Inventory.Bow]} " + Localization.Get(session, "menu_item_bows"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Stick]} " + Localization.Get(session, "inventory_category_sticks"),
+            RegisterButton($"{Emojis.inventory[Inventory.Stick]} " + Localization.Get(session, "menu_item_sticks"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Helmet]} " + Localization.Get(session, "inventory_category_helmets"),
+            RegisterButton($"{Emojis.inventory[Inventory.Helmet]} " + Localization.Get(session, "menu_item_helmets"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Armor]} " + Localization.Get(session, "inventory_category_armors"),
+            RegisterButton($"{Emojis.inventory[Inventory.Armor]} " + Localization.Get(session, "menu_item_armors"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Boots]} " + Localization.Get(session, "inventory_category_boots"),
+            RegisterButton($"{Emojis.inventory[Inventory.Boots]} " + Localization.Get(session, "menu_item_boots"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Shield]} " + Localization.Get(session, "inventory_category_shields"),
+            RegisterButton($"{Emojis.inventory[Inventory.Shield]} " + Localization.Get(session, "menu_item_shields"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Amulet]} " + Localization.Get(session, "inventory_category_amulets"),
+            RegisterButton($"{Emojis.inventory[Inventory.Amulet]} " + Localization.Get(session, "menu_item_amulets"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Ring]} " + Localization.Get(session, "inventory_category_rings"),
+            RegisterButton($"{Emojis.inventory[Inventory.Ring]} " + Localization.Get(session, "menu_item_rings"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Poison]} " + Localization.Get(session, "inventory_category_poisons"),
+            RegisterButton($"{Emojis.inventory[Inventory.Poison]} " + Localization.Get(session, "menu_item_poisons"),
                 null);
-            RegisterButton($"{Emojis.inventory[Inventory.Spells]} " + Localization.Get(session, "inventory_category_spells"),
+            RegisterButton($"{Emojis.inventory[Inventory.Spells]} " + Localization.Get(session, "menu_item_spells"),
                 null);
             RegisterButton($"{Emojis.elements[Element.Back]} " + Localization.Get(session, "menu_item_backButton"),
                 () => new TownCharacterDialog(session).Start());
