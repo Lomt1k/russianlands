c97bcf40745d0eb01d1de0900d9b1eb376af956d
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-05-31 06:49:24 +0300

    Randomizer: added random percentage logic

--- Scripts/Utils/Randomizer.cs
+++ Scripts/Utils/Randomizer.cs
@@ -5,5 +5,28 @@ namespace TextGameRPG.Scripts.Utils
     public static class Randomizer
     {
         public static Random random = new Random();
+
+        public static bool TryPercentage(int percent)
+        {
+            if (percent < 1)
+                return false;
+            if (percent > 99)
+                return true;
+
+            var result = random.Next(100);
+            return result < percent;
+        }
+
+        public static bool TryPercentage(double percent)
+        {
+            if (percent < double.Epsilon)
+                return false;
+            if (percent >= 100)
+                return true;
+
+            var result = random.NextDouble() * 100;
+            return result < percent;
+        }
+
     }
 }
