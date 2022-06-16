1e7689acfb0f48cc907cba5486a7c25010d55571
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-06-16 07:24:52 +0300

    LocationsHolder

--- Scripts/GameCore/Locations/Location.cs
+++ Scripts/GameCore/Locations/Location.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace TextGameRPG.Scripts.GameCore.Locations
 {
     internal class Location
     {
+        public LocationType type { get; }
+        public LocationData data { get; }
+
+        public Location(LocationType _type)
+        {
+            type = _type;
+            data = GameDataBase.GameDataBase.instance.locations[(int)type];
+        }
+
     }
+
 }
---
+++ Scripts/GameCore/Locations/LocationsHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace TextGameRPG.Scripts.GameCore.Locations
+{
+    internal static class LocationsHolder
+    {
+        private static Dictionary<LocationType, Location> _locations;
+
+        static LocationsHolder()
+        {
+            _locations = new Dictionary<LocationType, Location>();
+            var allData = GameDataBase.GameDataBase.instance.locations.GetAllData();
+            foreach (var locationData in allData)
+            {
+                var type = (LocationType)locationData.id;
+                var location = new Location(type);
+                _locations.Add(type, location);
+            }
+        }
+
+        public static Location Get(LocationType type)
+        {
+            return _locations[type];
+        }
+
+    }
+}
