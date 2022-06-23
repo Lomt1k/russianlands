9278b7090c728a8c2ce6228f8a331d73708f0605
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-06-23 08:30:12 +0300

    quest editor (in progress)

--- ViewModels/Editor/QuestsEditor/StageWithReplicaViewModel.cs
+++ ViewModels/Editor/QuestsEditor/StageWithReplicaViewModel.cs
@@ -6,15 +6,18 @@ using TextGameRPG.Scripts.GameCore.Quests.Characters;
 using TextGameRPG.Scripts.GameCore.Quests.QuestStages;
 using ReactiveUI;
 using System.Linq;
+using System.Reactive;
 
 namespace TextGameRPG.ViewModels.Editor.QuestsEditor
 {
     public class StageWithReplicaViewModel : ViewModelBase
     {
         private EnumValueModel<CharacterType> _selectedCharacter;
+        private Answer? _selectedAnswer;
 
         public QuestStageWithReplica stage { get; }
         public ObservableCollection<EnumValueModel<CharacterType>> characters { get; }
+        public ObservableCollection<Answer> answers { get; } = new ObservableCollection<Answer>();
         public EnumValueModel<CharacterType> selectedCharacter
         {
             get => _selectedCharacter;
@@ -24,12 +27,45 @@ namespace TextGameRPG.ViewModels.Editor.QuestsEditor
                 stage.replica.characterType = value.value;
             }
         }
+        public Answer? selectedAnswer
+        {
+            get => _selectedAnswer;
+            set => this.RaiseAndSetIfChanged(ref _selectedAnswer, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> addNewAnswerCommand { get; }
+        public ReactiveCommand<Unit, Unit> removeAnswerCommand { get; }
 
         public StageWithReplicaViewModel(QuestStageWithReplica stage)
         {
             this.stage = stage;
             characters = EnumValueModel<CharacterType>.CreateCollection();
             _selectedCharacter = characters.First(x => x.value == stage.replica.characterType);
+
+            foreach (var answer in stage.replica.answers)
+            {
+                answers.Add(answer);
+            }
+
+            addNewAnswerCommand = ReactiveCommand.Create(AddNewAnswer);
+            removeAnswerCommand = ReactiveCommand.Create(RemoveSelectedAnswer);
+        }
+
+        private void AddNewAnswer()
+        {
+            var answer = new Answer();
+            stage.replica.answers.Add(answer);
+            answers.Add(answer);
+        }
+
+        private void RemoveSelectedAnswer()
+        {
+            if (_selectedAnswer == null)
+                return;
+
+            var answer = _selectedAnswer;
+            stage.replica.answers.Remove(answer);
+            answers.Remove(answer);
         }
 
     }
