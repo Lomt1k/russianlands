c01fc475f1ac2bf58d01d2e58fdd0e6e536f7c5a
Vladimir Yashin <vladimir.yashin@mytona.com>
2022-09-17 20:14:40 +0300

    production buildings logic (in progress)

---
+++ Scripts/GameCore/Buildings/ProductionBuildingBase.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextGameRPG.Scripts.GameCore.Buildings.Data;
+using TextGameRPG.Scripts.GameCore.Resources;
+using TextGameRPG.Scripts.TelegramBot;
+using TextGameRPG.Scripts.TelegramBot.DataBase.SerializableData;
+using TextGameRPG.Scripts.TelegramBot.Sessions;
+
+namespace TextGameRPG.Scripts.GameCore.Buildings
+{
+    public abstract class ProductionBuildingBase : BuildingBase
+    {
+        public abstract ResourceType resourceType { get; }
+        public abstract int resourceLimitForZeroLevel { get; }
+
+        public string resourcePrefix;
+
+        public ProductionBuildingBase()
+        {
+            resourcePrefix = Emojis.resources[resourceType];
+        }
+
+        public abstract byte GetFirstWorkerLevel(ProfileBuildingsData data);
+        public abstract byte GetSecondWorkerLevel(ProfileBuildingsData data);
+        public abstract void SetFirstWorkerLevel(ProfileBuildingsData data, byte level);
+        public abstract void SetSecondWorkerLevel(ProfileBuildingsData data, byte level);
+        protected abstract long GetStartFarmTime(ProfileBuildingsData data);
+        protected abstract void SetStartFarmTime(ProfileBuildingsData data, long startConstructionTime);
+
+        public int GetCurrentLevelResourceLimit(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            if (currentLevel < 1)
+                return resourceLimitForZeroLevel;
+
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel - 1];
+            return levelInfo.resourceStorageLimit;
+        }
+
+        public int GetCurrentLevelFirstWorkerProductionPerHour(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            if (currentLevel < 1)
+                return 0;
+
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel - 1];
+            var basisPoint = (float)levelInfo.productionPerHour / 2;
+            var productionPerHour = GetFirstWorkerLevel(data) * levelInfo.bonusPerWorkerLevel + basisPoint;
+            return (int)productionPerHour;
+        }
+
+        public int GetCurrentLevelSecondWorkerProductionPerHour(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            if (currentLevel < 1)
+                return 0;
+
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel - 1];
+            var basisPoint = (float)levelInfo.productionPerHour / 2;
+            var productionPerHour = GetSecondWorkerLevel(data) * levelInfo.bonusPerWorkerLevel + basisPoint;
+            return (int)productionPerHour;
+        }
+
+        public int GetNextLevelResourceLimit(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel];
+            return levelInfo.resourceStorageLimit;
+        }
+
+        public int GetNextLevelFirstWorkerProductionPerHour(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel];
+            var basisPoint = (float)levelInfo.productionPerHour / 2;
+            var productionPerHour = GetFirstWorkerLevel(data) * levelInfo.bonusPerWorkerLevel + basisPoint;
+            return (int)productionPerHour;
+        }
+
+        public int GetNextLevelSecondWorkerProductionPerHour(ProfileBuildingsData data)
+        {
+            var currentLevel = GetCurrentLevel(data);
+            var levelInfo = (ProductionLevelInfo)buildingData.levels[currentLevel];
+            var basisPoint = (float)levelInfo.productionPerHour / 2;
+            var productionPerHour = GetSecondWorkerLevel(data) * levelInfo.bonusPerWorkerLevel + basisPoint;
+            return (int)productionPerHour;
+        }
+
+        public override string GetCurrentLevelInfo(GameSession session, ProfileBuildingsData data)
+        {
+            //var sb = new StringBuilder();
+            //sb.AppendLine(Localization.Get(session, $"building_{buildingType}_description"));
+
+            //sb.AppendLine();
+            //sb.AppendLine(Localization.Get(session, "building_storage_capacity_header"));
+            //var playerResources = session.player.resources;
+            //var currentValue = playerResources.GetValue(resourceType).View();
+            //var limitValue = GetCurrentLevelResourceLimit(data).View();
+            //var capacity = $"{resourcePrefix} {currentValue} / {limitValue}";
+            //sb.Append(capacity);
+
+            //return sb.ToString();
+            return string.Empty;
+        }
+
+        public override string GetNextLevelInfo(GameSession session, ProfileBuildingsData data)
+        {
+            //var sb = new StringBuilder();
+            //sb.AppendLine(Localization.Get(session, $"building_{buildingType}_description"));
+
+            //sb.AppendLine();
+            //sb.AppendLine(Localization.Get(session, "building_storage_capacity_header"));
+            //var currentLimit = GetCurrentLevelResourceLimit(data);
+            //var nextLimit = GetNextLevelResourceLimit(data);
+            //var delta = nextLimit - currentLimit;
+            //var capacity = $"{resourcePrefix} {nextLimit.View()} (<i>+{delta.View()}</i>)";
+            //sb.Append(capacity);
+
+            //return sb.ToString();
+            return string.Empty;
+        }
+    }
+}
--- Scripts/TelegramBot/DataBase/SerializableData/ProfileBuildingsData.cs
+++ Scripts/TelegramBot/DataBase/SerializableData/ProfileBuildingsData.cs
@@ -23,6 +23,76 @@ namespace TextGameRPG.Scripts.TelegramBot.DataBase.SerializableData
         public byte woodStorageLevel;
         public long woodStorageStartConstructionTime;
 
+        // --- Добыча ресурсов: Золото
+        public byte goldProdFirstLevel = 1;
+        public long goldProdFirstStartConstructionTime;
+        public long goldProdFirstStartFarmTime;
+        public int goldProdFirstWorkerFirst;
+        public int goldProdFirstWorkerSecond;
+
+        public byte goldProdSecondLevel;
+        public long goldProdSecondStartConstructionTime;
+        public long goldProdSecondStartFarmTime;
+        public int goldProdSecondWorkerFirst;
+        public int goldProdSecondWorkerSecond;
+
+        public byte goldProdThirdLevel;
+        public long goldProdThirdStartConstructionTime;
+        public long goldProdThirdStartFarmTime;
+        public int goldProdThirdWorkerFirst;
+        public int goldProdThirdWorkerSecond;
+
+        // --- Добыча ресурсов: Еда
+        public byte foodProdFirstLevel;
+        public long foodProdFirstStartConstructionTime;
+        public long foodProdFirstStartFarmTime;
+        public int foodProdFirstWorkerFirst;
+        public int foodProdFirstWorkerSecond;
+
+        public byte foodProdSecondLevel;
+        public long foodProdSecondStartConstructionTime;
+        public long foodProdSecondStartFarmTime;
+        public int foodProdSecondWorkerFirst;
+        public int foodProdSecondWorkerSecond;
+
+        public byte foodProdThirdLevel;
+        public long foodProdThirdStartConstructionTime;
+        public long foodProdThirdStartFarmTime;
+        public int foodProdThirdWorkerFirst;
+        public int foodProdThirdWorkerSecond;
+
+        // --- Добыча ресурсов: Травы
+        public byte herbsProdFirstLevel;
+        public long herbsProdFirstStartConstructionTime;
+        public long herbsProdFirstStartFarmTime;
+        public int herbsProdFirstWorkerFirst;
+        public int herbsProdFirstWorkerSecond;
+
+        public byte herbsProdSecondLevel;
+        public long herbsProdSecondStartConstructionTime;
+        public long herbsProdSecondStartFarmTime;
+        public int herbsProdSecondWorkerFirst;
+        public int herbsProdSecondWorkerSecond;
+
+        public byte herbsProdThirdLevel;
+        public long herbsProdThirdStartConstructionTime;
+        public long herbsProdThirdStartFarmTime;
+        public int herbsProdThirdWorkerFirst;
+        public int herbsProdThirdWorkerSecond;
+
+        // --- Добыча ресурсов: Дерево
+        public byte woodProdFirstLevel;
+        public long woodProdFirstStartConstructionTime;
+        public long woodProdFirstStartFarmTime;
+        public int woodProdFirstWorkerFirst;
+        public int woodProdFirstWorkerSecond;
+
+        public byte woodProdSecondLevel;
+        public long woodProdSecondStartConstructionTime;
+        public long woodProdSecondStartFarmTime;
+        public int woodProdSecondWorkerFirst;
+        public int woodProdSecondWorkerSecond;
+
 
         public static TableColumn[] GetTableColumns()
         {
@@ -44,6 +114,76 @@ namespace TextGameRPG.Scripts.TelegramBot.DataBase.SerializableData
                 new TableColumn("herbsStorageStartConstructionTime", "INTEGER", "0"),
                 new TableColumn("woodStorageLevel", "INTEGER", "0"),
                 new TableColumn("woodStorageStartConstructionTime", "INTEGER", "0"),
+
+                // --- Добыча ресурсов: Золото
+                new TableColumn("goldProdFirstLevel", "INTEGER", "1"),
+                new TableColumn("goldProdFirstStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("goldProdFirstStartFarmTime", "INTEGER", "0"),
+                new TableColumn("goldProdFirstWorkerFirst", "INTEGER", "0"),
+                new TableColumn("goldProdFirstWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("goldProdSecondLevel", "INTEGER", "0"),
+                new TableColumn("goldProdSecondStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("goldProdSecondStartFarmTime", "INTEGER", "0"),
+                new TableColumn("goldProdSecondWorkerFirst", "INTEGER", "0"),
+                new TableColumn("goldProdSecondWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("goldProdThirdLevel", "INTEGER", "0"),
+                new TableColumn("goldProdThirdStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("goldProdThirdStartFarmTime", "INTEGER", "0"),
+                new TableColumn("goldProdThirdWorkerFirst", "INTEGER", "0"),
+                new TableColumn("goldProdThirdWorkerSecond", "INTEGER", "0"),
+
+                // --- Добыча ресурсов: Еда
+                new TableColumn("foodProdFirstLevel", "INTEGER", "0"),
+                new TableColumn("foodProdFirstStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("foodProdFirstStartFarmTime", "INTEGER", "0"),
+                new TableColumn("foodProdFirstWorkerFirst", "INTEGER", "0"),
+                new TableColumn("foodProdFirstWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("foodProdSecondLevel", "INTEGER", "0"),
+                new TableColumn("foodProdSecondStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("foodProdSecondStartFarmTime", "INTEGER", "0"),
+                new TableColumn("foodProdSecondWorkerFirst", "INTEGER", "0"),
+                new TableColumn("foodProdSecondWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("foodProdThirdLevel", "INTEGER", "0"),
+                new TableColumn("foodProdThirdStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("foodProdThirdStartFarmTime", "INTEGER", "0"),
+                new TableColumn("foodProdThirdWorkerFirst", "INTEGER", "0"),
+                new TableColumn("foodProdThirdWorkerSecond", "INTEGER", "0"),
+
+                // --- Добыча ресурсов: Травы
+                new TableColumn("herbsProdFirstLevel", "INTEGER", "0"),
+                new TableColumn("herbsProdFirstStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("herbsProdFirstStartFarmTime", "INTEGER", "0"),
+                new TableColumn("herbsProdFirstWorkerFirst", "INTEGER", "0"),
+                new TableColumn("herbsProdFirstWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("herbsProdSecondLevel", "INTEGER", "0"),
+                new TableColumn("herbsProdSecondStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("herbsProdSecondStartFarmTime", "INTEGER", "0"),
+                new TableColumn("herbsProdSecondWorkerFirst", "INTEGER", "0"),
+                new TableColumn("herbsProdSecondWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("herbsProdThirdLevel", "INTEGER", "0"),
+                new TableColumn("herbsProdThirdStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("herbsProdThirdStartFarmTime", "INTEGER", "0"),
+                new TableColumn("herbsProdThirdWorkerFirst", "INTEGER", "0"),
+                new TableColumn("herbsProdThirdWorkerSecond", "INTEGER", "0"),
+
+                // --- Добыча ресурсов: Дерево
+                new TableColumn("woodProdFirstLevel", "INTEGER", "0"),
+                new TableColumn("woodProdFirstStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("woodProdFirstStartFarmTime", "INTEGER", "0"),
+                new TableColumn("woodProdFirstWorkerFirst", "INTEGER", "0"),
+                new TableColumn("woodProdFirstWorkerSecond", "INTEGER", "0"),
+
+                new TableColumn("woodProdSecondLevel", "INTEGER", "0"),
+                new TableColumn("woodProdSecondStartConstructionTime", "INTEGER", "0"),
+                new TableColumn("woodProdSecondStartFarmTime", "INTEGER", "0"),
+                new TableColumn("woodProdSecondWorkerFirst", "INTEGER", "0"),
+                new TableColumn("woodProdSecondWorkerSecond", "INTEGER", "0"),
             };
         }
 
