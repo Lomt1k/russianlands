33d211c10b59fe23046773615b057e7b103c8653
Vladimir Yashin <vladimir.yashin@mytona.com>
2023-05-06 16:41:01 +0300

    Arena Leagues (first commit)

---
+++ Scripts/GameCore/Services/Arena/ArenaLeague.cs
@@ -0,0 +1,19 @@
+﻿
+namespace MarkOne.Scripts.GameCore.Services.Arena;
+public enum ArenaLeague
+{
+    HALL_3,
+    HALL_4,
+    HALL_5_START,
+    HALL_5_MID,
+    HALL_5_END,
+    HALL_6_START,
+    HALL_6_MID,
+    HALL_6_END,
+    HALL_7_START,
+    HALL_7_MID,
+    HALL_7_END,
+    HALL_8_START,
+    HALL_8_MID,
+    HALL_8_END,
+}
