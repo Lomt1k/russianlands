9e441e6f746bb8bdc87f9a2989100892a1631dcb
Vladimir Yashin <vladimir.yashin@mytona.com>
2023-05-17 08:44:07 +0300

    Arena Dialog (commit #4)

--- Scripts/GameCore/Dialogs/Town/Map/Arena/ArenaDialog.cs
+++ Scripts/GameCore/Dialogs/Town/Map/Arena/ArenaDialog.cs
@@ -6,6 +6,8 @@ using MarkOne.Scripts.GameCore.Dialogs.Resources;
 using MarkOne.Scripts.GameCore.Localizations;
 using MarkOne.Scripts.GameCore.Resources;
 using MarkOne.Scripts.GameCore.Sessions;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,7 @@ namespace MarkOne.Scripts.GameCore.Dialogs.Town.Map.Arena;
 public class ArenaDialog : DialogBase
 {
     private static readonly ResourceData ticketPrice = new ResourceData(ResourceId.ArenaTicket, 1);
+    private static readonly byte targetBattlesCount = gameDataHolder.arenaSettings.battlesInMatch;
 
     public ArenaDialog(GameSession _session) : base(_session)
     {
@@ -20,6 +23,7 @@ public class ArenaDialog : DialogBase
 
     public override async Task Start()
     {
+        ClearButtons();
         var sb = new StringBuilder()
             .AppendLine(Emojis.ButtonArena + Localization.Get(session, "menu_item_arena").Bold())
             .AppendLine();
@@ -28,7 +32,6 @@ public class ArenaDialog : DialogBase
         if (hasArenaProgress)
         {
             var arenaProgress = session.profile.dynamicData.arenaProgress.EnsureNotNull();
-            var targetBattlesCount = gameDataHolder.arenaSettings.battlesInMatch;
             var registrationType = arenaProgress.byTicket
                 ? Localization.Get(session, "dialog_arena_registered_by_ticket")
                 : Localization.Get(session, "dialog_arena_registered_by_food");
@@ -57,7 +60,7 @@ public class ArenaDialog : DialogBase
             RegisterButton(ticketPrice.GetCompactView(shortView: false), () => TryStartNewMatch(byTicket: true));
         }
 
-        RegisterButton(Emojis.ButtonMarket + Localization.Get(session, "dialog_arena_shop_button"), null);
+        RegisterButton(Emojis.ButtonMarket + Localization.Get(session, "dialog_arena_shop_button"), null); // заглушка
         RegisterBackButton(Localization.Get(session, "menu_item_map") + Emojis.ButtonMap, () => new MapDialog(session).Start());
         RegisterTownButton(isDoubleBack: true);
 
@@ -87,6 +90,7 @@ public class ArenaDialog : DialogBase
             }
             sb.Append(Emojis.ProgressBarEmpty);
         }
+        sb.Append($" {results.Count} / {battlesCount}");
 
         return sb.ToString();
     }
@@ -121,7 +125,47 @@ public class ArenaDialog : DialogBase
 
     private async Task StartNextBattle()
     {
-        // TODO
+        var arenaProgress = session.profile.dynamicData.arenaProgress.EnsureNotNull();
+        var canWinAllBattles = arenaProgress.results.Count(x => x.result != BattleResult.Win) == 0;
+
+        var sb = new StringBuilder()
+            .AppendLine(Localization.Get(session, "dialog_arena_match_making_description"))
+            .AppendLine()
+            .AppendLine(Localization.Get(session, "dialog_arena_match_making_approximate_time", 180)); // заглушка
+
+        if (canWinAllBattles)
+        {
+            sb.AppendLine();
+            sb.AppendLine(Localization.Get(session, "dialog_arena_match_making_challenge", targetBattlesCount));
+            sb.AppendLine(GetRewardsForWinAllBattles(arenaProgress.byTicket).GetLocalizedView(session));
+        }
+
+        ClearButtons();
+        RegisterButton(Emojis.ElementCancel + Localization.Get(session, "dialog_arena_cancel_match_making_button"), TryCancelNextBattle);
+        await SendDialogMessage(sb, GetOneLineKeyboard()).FastAwait();
+        // TODO: Register player in match making
+    }
+
+    private IEnumerable<ResourceData> GetRewardsForWinAllBattles(bool byTicket)
+    {
+        var rewardSetiings = byTicket
+            ? gameDataHolder.arenaSettings.battleRewardsForTicket
+            : gameDataHolder.arenaSettings.battleRewardsForFood;
+
+        if (rewardSetiings.ticketsForWinAllBattles > 0)
+        {
+            yield return new ResourceData(ResourceId.ArenaTicket, rewardSetiings.ticketsForWinAllBattles);
+        }
+        if (rewardSetiings.chipsForWinAllBattles > 0)
+        {
+            yield return new ResourceData(ResourceId.ArenaChip, rewardSetiings.chipsForWinAllBattles);
+        }
+    }
+
+    private async Task TryCancelNextBattle()
+    {
+        // TODO: Check cancel available
+        await Start().FastAwait();
     }
 
 }
