7f6b1ffa106013044bd191320666dd982921669e
Vladimir Yashin <vladimir.yashin@mytona.com>
2023-06-21 09:06:16 +0300

    Http Admin Service (Add Item, final commit)

--- Scripts/GameCore/Http/AdminService/HtmlPages/PlayerControl/AddItemPage.cs
+++ Scripts/GameCore/Http/AdminService/HtmlPages/PlayerControl/AddItemPage.cs
@@ -12,6 +12,8 @@ using MarkOne.Scripts.GameCore.Items;
 using MarkOne.Scripts.GameCore.Localizations;
 using MarkOne.Scripts.GameCore.Sessions;
 using MarkOne.Scripts.GameCore.Services;
+using MarkOne.Scripts.GameCore.Items.Generators;
+using System.Linq;
 
 namespace MarkOne.Scripts.GameCore.Http.AdminService.HtmlPages.PlayerControl;
 internal class AddItemPage : IHtmlPage
@@ -75,14 +77,10 @@ internal class AddItemPage : IHtmlPage
         var document = HtmlHelper.CreateDocument("Add Item");
         document["html"]["body"].AddProperties(StylesHelper.CenterScreenParent());
 
-        var centerScreenBlock = new HTag("div", StylesHelper.CenterScreenBlock(700, 700));
+        var centerScreenBlock = new HTag("div", StylesHelper.CenterScreenBlock(1200, 900));
         document["html"]["body"].Add(centerScreenBlock);
 
-        var resourceIds = new List<string>();
-        foreach (var id in Enum.GetValues<ResourceId>())
-        {
-            resourceIds.Add(id.ToString());
-        }
+        centerScreenBlock.Add(GetItemCodesInfo());
 
         var form = new HtmlFormBuilder(localPath)
             .AddHeader($"Add Item (ID {parsedTelegramId})")
@@ -164,6 +162,123 @@ internal class AddItemPage : IHtmlPage
     {
         return localPath + $"?page=playerSearch&telegramId={query["telegramId"]}"
             + (query["showActivePlayers"] is not null ? "&showActivePlayers=" : string.Empty);
+    }
+
+    private HTag GetItemCodesInfo()
+    {
+        var itemTypesInfo = new HTag("div", new HProp("style", "float: left;"))
+        {
+            { "h3", "Items" },
+            new HTag("table", new HProp("frame", "hsides"))
+            {
+                CreateTableRow(ItemType.Sword.GetEmoji() + "SW"),
+                CreateTableRow(ItemType.Bow.GetEmoji() + "BO"),
+                CreateTableRow(ItemType.Stick.GetEmoji() + "ST"),
+                CreateTableRow(ItemType.Scroll.GetEmoji() + "SC"),
+                CreateTableRow(ItemType.Armor.GetEmoji() + "AR"),
+                CreateTableRow(ItemType.Helmet.GetEmoji() + "HE"),
+                CreateTableRow(ItemType.Boots.GetEmoji() + "BT"),
+                CreateTableRow(ItemType.Shield.GetEmoji() + "SH"),
+                CreateTableRow(ItemType.Amulet.GetEmoji() + "AM"),
+                CreateTableRow(ItemType.Ring.GetEmoji() + "RI"),
+            }
+        };
+
+        var townhallsInfo = new HTag("div", new HProp("style", "float: left; margin-left: 30px;"))
+        {
+            { "h3", "Townhalls" },
+            new HTag("table", new HProp("frame", "hsides")),
+        };
+        foreach (var townhall in ItemGenerationHelper.itemGradesByTownHall.Keys)
+        {
+            townhallsInfo["table"].Add(CreateTableRow(townhall.ToString()));
+        }
+
+        var gradesInfo = new HTag("div", new HProp("style", "float: left; margin-left: 30px;"))
+        {
+            { "h3", "Grades" },
+            new HTag("table", new HProp("frame", "hsides")),
+        };
+        foreach (var gradesArray in ItemGenerationHelper.itemGradesByTownHall.Values)
+        {
+            var grades = string.Empty;
+            foreach (var grade in gradesArray.ToHashSet())
+            {
+                grades += $"G{grade} ";
+            }
+            gradesInfo["table"].Add(CreateTableRow(grades));
+        }
+
+        var raritiesInfo = new HTag("div", new HProp("style", "float: left; margin-left: 30px;"))
+        {
+            { "h3", "Rarities" },
+            new HTag("table", new HProp("frame", "hsides")),
+            { "h3", "Mana Cost (scrolls)" },
+        };
+        foreach (var rarity in Enum.GetValues<Rarity>())
+        {
+            raritiesInfo["table"].Add(CreateTableRow($"R{(int)rarity} - {rarity}"));
+        }
+        var manaCostsTable = new HTag("table", new HProp("frame", "hsides"))
+        {
+            CreateTableRow("M2"),
+            CreateTableRow("M3"),
+            CreateTableRow("M4"),
+            CreateTableRow("M5"),
+        };
+        raritiesInfo.Add(manaCostsTable);
+
+        var propertiesInfo = new HTag("div", new HProp("style", "float: left; margin-left: 30px;"))
+        {
+            { "h3", "Properties & Abilities" },
+            new HTag("table", new HProp("frame", "hsides"))
+            {
+                CreateTableRow("DF - fire damage / resistance"),
+                CreateTableRow("DC - cold damage / resistance"),
+                CreateTableRow("DL - lightning damage / resistance"),
+                CreateTableRow("P2 - increase max health (ring / amulet)"),
+                CreateTableRow("A3 - restore health every turn (ring / amulet)"),
+                CreateTableRow("A4 - add mana every turn (ring / amulet)"),
+                CreateTableRow("A5 - sword block keyword (sword)"),
+                CreateTableRow("A6 - bow last shot keyword (bow)"),
+                CreateTableRow("A7 - add arrow keyword (sword)"),
+                CreateTableRow("A8 - steal mana keyword (sword / bow / stick)"),
+                CreateTableRow("A9 - fire damage keyword (sword / bow / stick)"),
+                CreateTableRow("A10 - cold damage keyword (sword / bow / stick)"),
+                CreateTableRow("A11 - lightning damage keyword (sword / bow / stick)"),
+                CreateTableRow("A12 - rage keyword (sword / stick)"),
+                CreateTableRow("A13 - finishing keyword (sword / bow / stick)"),
+                CreateTableRow("A14 - absorption keyword (sword / bow / stick)"),
+                CreateTableRow("A15 - add mana keyword (stick)"),
+                CreateTableRow("A16 - stun keyword (sword / bow / stick)"),
+                CreateTableRow("A17 - sanctions keyword (sword / bow / stick)"),
+            }
+        };
+
+        var examplesInfo = new HTag("div", new HProp("style", "clear: both; margin-top:60px;"))
+        { 
+            { "p", "<b>Examples:</b> SW3G5R3DFA7A16, RI8G10R2P2A4, SC7G4R3M5DL, AR8G2R3DFDCDL" }
+        };
+
+        var itemCodesInfo = new HTag("div", new HProp("align", "left"))
+        {
+            itemTypesInfo,
+            townhallsInfo,
+            gradesInfo,
+            raritiesInfo,
+            propertiesInfo,
+            examplesInfo,
+        };
+        return itemCodesInfo;
+    }
+
+    private HTag CreateTableRow(string text)
+    {
+        var row = new HTag("tr")
+        {
+            { "td", text, new HProp("align", "left") }
+        };
+        return row;
     }
 
 }
