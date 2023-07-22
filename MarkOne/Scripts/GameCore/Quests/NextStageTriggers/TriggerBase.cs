5538fe3d29152dae63edf0f1a559495961a62a1e
Vladimir Yashin <vladimir.yashin@mytona.com>
2023-07-22 17:23:28 +0300

    quest hotfix

--- MarkOne/Scripts/GameCore/Quests/NextStageTriggers/TriggerBase.cs
+++ MarkOne/Scripts/GameCore/Quests/NextStageTriggers/TriggerBase.cs
@@ -7,7 +7,7 @@ namespace MarkOne.Scripts.GameCore.Quests.NextStageTriggers;
 public abstract class TriggerBase
 {
     public abstract TriggerType triggerType { get; }
-    public int nextStage = -1;
+    public int nextStage { get; set; } = -1;
 
     public abstract bool TryInvoke();
 }
