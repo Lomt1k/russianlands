05608393858bcf6b4f5b68897e15949259ca5688
Vladimir Yashin <vladimir.yashin@mytona.com>
2023-11-10 15:25:54 +0300

    Up project version to 1.2

--- MarkOne/ProjectVersion.cs
+++ MarkOne/ProjectVersion.cs
@@ -9,7 +9,7 @@
 
 internal readonly struct ProjectVersion
 {
-    public static readonly ProjectVersion Current = new ProjectVersion("1.1.0");
+    public static readonly ProjectVersion Current = new ProjectVersion("1.2.0");
 
     public byte globalNumber { get; }
     public byte update { get; }
