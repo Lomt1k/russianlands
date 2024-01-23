b8bf1c3d4a56be46cbbfdc3d34cb0276bedb04fb
Vladimir Yashin <vladimir.yashin@mytona.com>
2024-01-23 21:11:39 +0300

    Revert "Исправлен редкий баг, когда бот мог отреагировать на ваше сообщение как на сообщение от другого игрока."
    This reverts commit a96809c12350979a97b1151eade2c681b74de761.

--- MarkOne/Scripts/GameCore/Dialogs/DialogPanelBase.cs
+++ MarkOne/Scripts/GameCore/Dialogs/DialogPanelBase.cs
@@ -255,7 +255,7 @@ public abstract class DialogPanelBase
     {
         if (callbackData.aliveQueryHash != _aliveQueryHash)
         {
-            await messageSender.AnswerQuery(queryId, Localization.Get(session, "invalid_query_answer"), session.cancellationToken).FastAwait();
+            await messageSender.AnswerQuery(queryId, Localization.Get(session, "invalid_query_answer")).FastAwait();
             return;
         }
 
--- MarkOne/Scripts/GameCore/Services/Sessions/GameSession.cs
+++ MarkOne/Scripts/GameCore/Services/Sessions/GameSession.cs
@@ -196,7 +196,7 @@ public class GameSession
 
     private async Task AnswerInvalidQueryAsync(string queryId)
     {
-        await messageSender.AnswerQuery(queryId, Localization.Get(this, "invalid_query_answer"), cancellationToken).FastAwait();
+        await messageSender.AnswerQuery(queryId, Localization.Get(this, "invalid_query_answer")).FastAwait();
     }
 
     private async Task OnStartNewSession(Update update)
